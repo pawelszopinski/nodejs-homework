4e8759174ecfdfc6aefa3bf38f45c903fdcf9fa4
Paweł Szopiński <szopinskipawel@gmail.com>
2023-10-21 13:40:01 +0200

    updated index and server.js

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
-import app from "./app.js";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+import routerAPI from "./api/index.js";
+app.use("/api/contacts", routerAPI);
+
+app.use((_, res, __) => {
+  res.json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/contacts",
+    data: "Not found",
+  });
 });
+
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const connection = async () => {
+  try {
+    await mongoose.connect(uriDb, { dbName: "db-contacts" });
+    console.log("Database connection successful");
+
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+connection();
