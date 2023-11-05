cc1ef17ec744701677a531080559131b36e6512a
Paweł Szopiński <szopinskipawel@gmail.com>
2023-11-05 13:32:50 +0100

    Added schema and bcrypt dep

---
+++ models/users.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bCrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
+
+userSchema.methods.setPassword = function (password) {
+  this.password = bCrypt.hashSync(password, bCrypt.genSaltSync(6));
+};
+
+userSchema.methods.validPassword = function (password) {
+  return bCrypt.compareSync(password, this.password);
+};
+
+export const User = mongoose.model("user", userSchema);
